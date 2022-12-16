b380bd25f2778e772f35e541560bab37f2398a0a
CiksGg <kambarbekovamantur81@gmail.com>
2022-12-16 11:47:07 +0600

    review models

--- review/models.py
+++ review/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from main.models import Post
+
+User = get_user_model()
+
+class Comment(models.Model):
+    author = models.ForeignKey(User,related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class LikePost(models.Model):
+    author = models.ForeignKey(User,related_name='post_likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,related_name='likes',on_delete=models.CASCADE)
+
+class LikeComment(models.Model):
+    author = models.ForeignKey(User, related_name='comment_likes', on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, related_name='likes', on_delete=models.CASCADE)
+    
